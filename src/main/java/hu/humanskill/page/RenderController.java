83dab0e94c87baf0734c52fc7b0706f0ed9d669d
Akos Asztalos <aztalos.akos@gmail.com>
2018-02-18 21:10:54 +0100

    properties files added

--- src/main/java/hu/humanskill/page/RenderController.java
+++ src/main/java/hu/humanskill/page/RenderController.java
@@ -5,12 +5,20 @@ import spark.Request;
 import spark.Response;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
+
 
 public class RenderController {
 
+
+    private static ResourceBundle text = ResourceBundle.getBundle("languages/en_GB");
+
     public static ModelAndView renderIndexPage(Request req, Response res) {
         Map params = new HashMap<>();
+        System.out.println(text.getString("menu1"));
+        params.put("menu1", text.getString("menu1"));
         return new ModelAndView(params, "index2");
     }
 }
