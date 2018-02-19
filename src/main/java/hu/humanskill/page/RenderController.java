4d25aa19f599f234fe1a90b2f5b9d6158e97f445
Akos Asztalos <aztalos.akos@gmail.com>
2018-02-20 00:05:45 +0100

    looks fin in chrome

--- src/main/java/hu/humanskill/page/RenderController.java
+++ src/main/java/hu/humanskill/page/RenderController.java
@@ -12,13 +12,12 @@ import java.util.ResourceBundle;
 
 public class RenderController {
 
-
-    private static ResourceBundle text = ResourceBundle.getBundle("languages/en_GB");
+    //private static ResourceBundle text = ResourceBundle.getBundle("languages/en_GB");
 
     public static ModelAndView renderIndexPage(Request req, Response res) {
         Map params = new HashMap<>();
-        System.out.println(text.getString("menu1"));
-        params.put("menu1", text.getString("menu1"));
-        return new ModelAndView(params, "index2");
+        //System.out.println(text.getString("menu1"));
+        //params.put("menu1", text.getString("menu1"));
+        return new ModelAndView(params, "index");
     }
 }
